e489a029f0db1153893f4ae7d6caa053d9c5703d
denis <denis@LAPTOP-Q9HNLE8L>
2022-12-05 17:05:30 -0800

    chuckl

---
+++ src/_01_chuckle_clicker/ChuckleClicker.java
@@ -0,0 +1,23 @@
+package _01_chuckle_clicker;
+
+import javax.swing.*;
+
+public class ChuckleClicker {
+
+	static void makeButtons()
+	{
+		
+	JOptionPane.showMessageDialog(null, "ptok");
+	
+	}
+	public static void main(String[] args) {
+	makeButtons();	
+	
+	}
+	
+	
+		
+		
+		
+	
+}
